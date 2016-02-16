5cc9c8107b218374ff1ccde7876bc12c781522fb
Syrus Mesdaghi <syrusm@prominentedge.com>
2016-02-16 12:01:33 -0500

    support for textarea and resize text area based on content height

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,19 +1,19 @@
 angular.module('vida.directives', [])
-  .directive('hideKeyboardOnEnter', function ($window) {
-    return {
-      restrict: 'A',
-      link: function (scope, element, attrs) {
-        if ($window.cordova && $window.cordova.plugins.Keyboard) {
-          element.bind('keyup', function (key) {
-            if (key.keyCode === 13) {
-              $window.cordova.plugins.Keyboard.close();
-              element[0].blur();
-            }
-          });
-        }
+.directive('hideKeyboardOnEnter', function ($window) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      if ($window.cordova && $window.cordova.plugins.Keyboard) {
+        element.bind('keyup', function (key) {
+          if (key.keyCode === 13) {
+            $window.cordova.plugins.Keyboard.close();
+            element[0].blur();
+          }
+        });
       }
-    };
-  })
+    }
+  };
+})
 
 .directive('errSrc', function() {
     return {
@@ -28,6 +28,25 @@ angular.module('vida.directives', [])
         if (!value && attrs.errSrc) {
           attrs.$set('src', attrs.errSrc);
         }
+      });
+    }
+  };
+})
+
+.directive('updateHeightOnChange', function() {
+  return {
+    link: function(scope, element, attrs) {
+      scope.updateHeight = function(){
+        element.css('height', '1px');
+        element.css('height', (20 + element.prop('scrollHeight')) + 'px');
+        console.log('heigh adjusted for elem: ', element);
+      };
+
+      scope.updateHeight();
+
+      scope.$watch(attrs.ngModel, function (v) {
+        scope.updateHeight();
+        console.log('value changed, new value is: ' + v);
       });
     }
   };
